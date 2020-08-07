88e9920138cbe3e31dce242daae97be0acdbed0a
legir <arigelh@gmail.com>
2020-08-07 19:04:26 +0800

    Added tools for the gem material, sword, pickaxe, axe, shovel, and hoe.

--- src/main/java/com/legir/testmod/util/RegistryHandler.java
+++ src/main/java/com/legir/testmod/util/RegistryHandler.java
@@ -4,8 +4,9 @@ import com.legir.testmod.TestMod;
 import com.legir.testmod.blocks.BlockItemBase;
 import com.legir.testmod.blocks.GemBlock;
 import com.legir.testmod.items.ItemBase;
+import com.legir.testmod.tools.ModItemTier;
 import net.minecraft.block.Block;
-import net.minecraft.item.Item;
+import net.minecraft.item.*;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
@@ -25,6 +26,12 @@ public class RegistryHandler {
     //Items
     public static final RegistryObject<Item> GEM = ITEMS.register("gem", ItemBase::new);
 
+    //Tools
+    public static final RegistryObject<SwordItem> GEM_SWORD = ITEMS.register("gem_sword", () -> new SwordItem(ModItemTier.GEM, 4, -2.4F, new Item.Properties().group(TestMod.TAB)));
+    public static final RegistryObject<PickaxeItem> GEM_PICKAXE = ITEMS.register("gem_pickaxe", () -> new PickaxeItem(ModItemTier.GEM, 0, -2.8F, new Item.Properties().group(TestMod.TAB)));
+    public static final RegistryObject<AxeItem> GEM_AXE = ITEMS.register("gem_axe", () -> new AxeItem(ModItemTier.GEM, 6, -3.1F, new Item.Properties().group(TestMod.TAB)));
+    public static final RegistryObject<ShovelItem> GEM_SHOVEL = ITEMS.register("gem_shovel", () -> new ShovelItem(ModItemTier.GEM, 0.5F, -3.0F, new Item.Properties().group(TestMod.TAB)));
+    public static final RegistryObject<HoeItem> GEM_HOE = ITEMS.register("gem_hoe", () -> new HoeItem(ModItemTier.GEM, -3,-1.0F, new Item.Properties().group(TestMod.TAB)));
 
     // Blocks
     public static final RegistryObject<Block> GEM_BLOCK = BLOCKS.register("gem_block", GemBlock::new);
